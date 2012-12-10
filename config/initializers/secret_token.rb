a25e0203208d9ace66c40af2d96c378e4648c5f9
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-12-10 08:28:06 -0800

    Added mongodb info and generated mongoid stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ShortUrl::Application.config.secret_token = '8de77fdab1142356893ecc3da6ce0d6f8163b51c979d3a10d74fda04e83ad2678d9c6946cfd8b5f2ca0529d0360c9697278dbd55431528611fa72c2629ffc82c'
+ShortUrl::Application.config.secret_token = 'adb0b7a8bb734f9f806f53f213ee557991df90a078601bb3fa5a8fd01fdda25b3fc3ca4f1c1bf57e7d4cdf60c685c8d82c059c9105b27728079afddf4321f3e7'
