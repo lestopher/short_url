85cb81574bce331d63060d546aabd7dd525e4b1c
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-12-10 23:55:53 -0800

    Functionality is mostly working, crc32 isn't working right now

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -6,16 +6,35 @@ class UrlsController < ApplicationController
   end
 
   def new
-    if params[:url].nil?
+  end
+
+  def create
+    if params[:url].length == 0
+      flash[:error] = "You need to enter a URL at the minimum."
       redirect_to root_path
+      return
     end
 
     url        = params[:url]
-    custom_url = params[:custom_url] || create_hashed_url(params[:url])
+    custom_url = params[:custom_url] || Url.create_hashed_url(params[:url])
 
-    
+    @msg       = Url.check_options(url, custom_url)
+logger.debug "msg hash: #{@msg}"
+    if @msg[:url][:count] > 0 && params[:force_cb] != 1
+      flash[:notice] = "There are currently #{msg[:count]} instances of the url #{url} in use, please consider using one of the following:"
+      @custom = msg[:url][:data]
+      redirect_to root_path
+      return
+    elsif @msg[:custom_url] == true
+      flash[:error] = "The custom url, #{custom_url} you have requested is already in use."
+      redirect_to root_path
+      return
+    else
+    end
 
-    url_obj = Url.new(full_url: url, hashed_url: custom_url)
+    url_obj = Url.create(full_url: url, hashed_url: custom_url)
+
+    redirect_to root_path, :notice => "Successfully created custom url: http://evi.io/#{custom_url}"
   end
 
   def edit
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,9 @@
 class Url
   include Mongoid::Document
-  field :full_url, type: String
-  field :hashed_url, type: String
-  field :created_by, type: String
-  field :created_date, type: DateTime, default: ->{ Time.now }
+  field :fu, as: :full_url, type: String
+  field :hu, as: :hashed_url, type: String
+  field :cb, as: :created_by, type: String
+  field :cd, as: :created_date, type: DateTime, default: ->{ Time.now }
 
   def self.create_hashed_url(url)
     require 'zlib'
@@ -11,6 +11,15 @@ class Url
   end
 
   def self.check_options(url, custom_url)
-    
+    msg = { :url => {} }
+
+    url_results = self.where(:full_name => url)
+    custom_url  = self.where(:hashed_url => custom_url)
+
+    msg[:url][:count] =  url_results.count
+    msg[:url][:data]  =  url_results
+    msg[:custom_url]  =  custom_url.exists?
+
+    return msg
   end
 end
