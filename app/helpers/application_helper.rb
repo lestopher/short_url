5ad7efbd55d7e32b3da6f4ddd8e0c52ace3b0b77
Christopher Nguyen <christoph.nguyen@gmail.com>
2012-12-14 01:00:34 -0800

    Added helper function site_name to globally set site name and added helpers to figure out what controller and action you're currently in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def site_name
+    return "localhost:3000"
+  end
+
+  def current_controller
+    params[:controller]
+  end
+
+  def current_action
+    params[:action]
+  end
 end
